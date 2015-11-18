5732da0e189cca7757ac3d46f02913ca690911f6
voduytuan <tuanmaster2012@gmail.com>
2015-11-18 11:30:45 +0700

    (feat) update new way of cd to opencv directory

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,7 +1,8 @@
-curl -L 'https://github.com/Itseez/opencv/tarball/2.4' | tar xvzf -
-mkdir -p Itseez-opencv-01b5971/release
-cd Itseez-opencv-01b5971/release
+mkdir -p /opencv
+curl -L 'https://github.com/Itseez/opencv/tarball/2.4' | tar xvzf - -C FOLDER --strip-components=1
+mkdir -p /opencv/release
+cd /opencv/release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D WITH_XINE=ON -D WITH_TBB=ON ..
 make && make install
 cd /
-rm -rf Itseez-opencv-01b5971
+rm -rf opencv

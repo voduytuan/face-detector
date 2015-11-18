f16cd5aee13e0493b00cd87b35400954ddf5fcb7
voduytuan <tuanmaster2012@gmail.com>
2015-11-18 10:29:30 +0700

    (feat) add crop action and define constance

--- www/index.php
+++ www/index.php
@@ -1,8 +1,11 @@
 <?php
 
 ini_set('display_errors', 1);
-
 set_time_limit(300);
+
+define('MAX_IMAGE_SIZE', 5 * 1024 * 1024);
+define('FACECOUNT_HAARCASCADE_XML', 'haarcascade_profileface.xml');
+define('FACEDETECT_HAARCASCADE_XML', 'haarcascade_profileface.xml');
 
 // Get action from url
 $action = '';
@@ -13,6 +16,9 @@ if (isset($_GET['action']) && $_GET['action'] != '') {
 switch ($action) {
     case 'count':
         doCount();
+        break;
+    case 'crop':
+        doCrop();
         break;
     default:
         doDetect();
@@ -30,7 +36,7 @@ function doCount()
 
     if (fileValidate($filepath, $error)) {
         $success = true;
-        $count = face_count($filepath, 'opencv-2.4.10-haarcascades/haarcascade_profileface.xml');
+        $count = face_count($filepath, 'opencv-2.4.10-haarcascades/' . FACECOUNT_HAARCASCADE_XML);
 
     } else {
         $message = $error;
@@ -47,6 +53,30 @@ function doCount()
 }
 
 /**
+ * smart crop photo to square ratio with focus on main faces
+ */
+function doCrop()
+{
+    if (fileValidate($filepath, $error)) {
+        $success = true;
+        $faces = face_detect($filepath, 'opencv-2.4.10-haarcascades/' . FACEDETECT_HAARCASCADE_XML);
+
+        //Check if found face, need to crop
+        if (!empty($faces)) {
+
+            echo 'Now, Crop it!';
+
+        } else {
+            header('HTTP/1.1 422 Unprocessable Entity', true, 422);
+            echo 'There is no face in uploaded image.';
+        }
+    } else {
+        header('HTTP/1.1 422 Unprocessable Entity', true, 422);
+        echo $message;
+    }
+}
+
+/**
  * Return all faces information in uploaded photo
  */
 function doDetect()
@@ -58,7 +88,7 @@ function doDetect()
 
     if (fileValidate($filepath, $error)) {
         $success = true;
-        $faces = face_detect($filepath, 'opencv-2.4.10-haarcascades/haarcascade_profileface.xml');
+        $faces = face_detect($filepath, 'opencv-2.4.10-haarcascades/' . FACEDETECT_HAARCASCADE_XML);
 
     } else {
         $message = $error;
@@ -91,9 +121,9 @@ function fileValidate(&$filepath, &$error)
             $pass = false;
             $error = 'Image must be in JPG format.';
 
-        } elseif ($_FILES['image']['size'] > 5 * 1024 * 1024) {
+        } elseif ($_FILES['image']['size'] > MAX_IMAGE_SIZE) {
             $pass = false;
-            $error = 'Image is too big, must be under 5MB.';
+            $error = 'Image is too big, must be under '.MAX_IMAGE_SIZE.' bytes.';
 
         } else {
             $filepath = $_FILES['image']['tmp_name'];
